fe2fdbb4d33bed43eb0104dcd320821becff2f34
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-22 22:14:59 +0530

    Update log.js

--- log.js
+++ log.js
@@ -1,16 +1,16 @@
-class log {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
+  class log {
+      constructor(x,y,width,height) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
+      display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+      }
     }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      rect(pos.x, pos.y, this.width, this.height);
-    }
-  }

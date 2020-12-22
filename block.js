33621f318284f8e4b17223a6981f9f680deec767
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-22 22:15:58 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,28 +1,36 @@
-class box {
-    constructor(x, y, width, height) {
-      var options = {
-          isStatic:false,
-          restitution:0.8,
-          friction:0.3,
-          density:1.0
-      }
+  class box {
+      constructor(x, y, width, height) {
+        var options = {
+            isStatic:false     ,
+            restitution:0.8,
+            friction:0.3,
+            density:1.0
+        }
 
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width; 
-      this.height = height;
-      
-      World.add(world, this.body); 
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width; 
+        this.height = height;
 
-   }
+        World.add(world, this.body); 
+
+        this.visible=250;
+    }
 
     display(){
-      var pos =this.body.position;
-      var angle = this.body.angle;
+      console.log(this.body.speed)
+
       push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      rect(0, 0, this.width, this.height);
+      if(this.body.speed<3){
+        var angle = this.body.angle;
+        rotate(angle);
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y, this.width, this.height);
+      }
+      else{
+        World.remove(world,this.body);
+        this.visible=this.visible-5;
+        tint(250, this.visible);
+      }
       pop();
     }
   }
